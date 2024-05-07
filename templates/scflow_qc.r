bfb9f2e1c5c46dd92b3ed1816d8fe0499fe5f7f8
Wojtek <ws420@ic.ac.uk>
2024-05-07 19:07:38 +0100

    removed redundant type conversions

--- templates/scflow_qc.r
+++ templates/scflow_qc.r
@@ -35,34 +35,36 @@ parse_args <- function(x){
 args <- {}
 args$key_colname <- "!{params.options.key_colname}"
 args$factor_vars <- "!{params.options.factor_vars}"
-args$min_library_size <- "!{params.options.min_library_size}"
+args$min_library_size <- !{params.options.min_library_size}
 args$max_library_size <- "!{params.options.max_library_size}"
-args$min_features <- "!{params.options.min_features}"
+args$min_features <- !{params.options.min_features}
 args$max_features <- "!{params.options.max_features}"
 args$max_mito <- "!{params.options.max_mito}"
-args$min_ribo <- "!{params.options.min_ribo}"
-args$max_ribo <- "!{params.options.max_ribo}"
-args$min_counts <- "!{params.options.min_counts}"
-args$min_cells <- "!{params.options.min_cells}"
+args$min_ribo <- !{params.options.min_ribo}
+args$max_ribo <- !{params.options.max_ribo}
+args$min_counts <- !{params.options.min_counts}
+args$min_cells <- !{params.options.min_cells}
 args$drop_unmapped <- "!{params.options.drop_unmapped}"
 args$drop_mito <- "!{params.options.drop_mito}"
 args$drop_ribo <- "!{params.options.drop_ribo}"
-args$nmads <- "!{params.options.nmads}"
+args$nmads <- !{params.options.nmads}
 args$find_singlets <- "!{params.options.find_singlets}"
 args$singlets_method <- "!{params.options.singlets_method}"
 args$vars_to_regress_out <- "!{params.options.vars_to_regress_out}"
-args$pca_dims <- "!{params.options.pca_dims}"
-args$var_features <- "!{params.options.var_features}"
-args$doublet_rate <- "!{params.options.doublet_rate}"
+args$pca_dims <- !{params.options.pca_dims}
+args$var_features <- !{params.options.var_features}
+args$doublet_rate <- !{params.options.doublet_rate}
 args$dpk <- "!{params.options.dpk}"
 args$pK <- "!{params.options.pK}"
 args$find_cells <- "!{params.options.find_cells}"
-args$lower <- "!{params.options.lower}"
+args$lower <- !{params.options.lower}
 args$retain <- "!{params.options.retain}"
-args$alpha_cutoff <- "!{params.options.alpha_cutoff}"
-args$niters <- "!{params.options.niters}"
+args$alpha_cutoff <- !{params.options.alpha_cutoff}
+args$niters <- !{params.options.niters}
 args$expect_cells <- "!{params.options.expect_cells}"
 args$species <- "!{params.options.species}"
+
+
 
 
 ### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
@@ -120,21 +122,6 @@ if (!is.null(args$factor_vars)) {
 } else {
   col_classes <- NA
 }
-
-args$nmads <- as.numeric(as.character(args$nmads))
-args$min_library_size <- as.numeric(as.character(args$min_library_size))
-args$min_features <- as.numeric(as.character(args$min_features))
-args$min_ribo <- as.numeric(as.character(args$min_ribo))
-args$max_ribo <- as.numeric(as.character(args$max_ribo))
-args$min_counts <- as.numeric(as.character(args$min_counts))
-args$min_cells <- as.numeric(as.character(args$min_cells))
-args$pca_dims <- as.numeric(as.character(args$pca_dims))
-args$var_features <- as.numeric(as.character(args$var_features))
-args$doublet_rate <- as.numeric(as.character(args$doublet_rate))
-args$lower <- as.numeric(as.character(args$lower))
-args$alpha_cutoff <- as.numeric(as.character(args$alpha_cutoff))
-args$niters <- as.numeric(as.character(args$niters))
-
 
 ##  ............................................................................
 ##  Start QC                                                                ####
